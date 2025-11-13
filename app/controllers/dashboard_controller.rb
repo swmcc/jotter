0e76244cedce9a4df42de650e257a78fff59ca5d
Stephen McCullough <me@swm.cc>
2025-11-13 22:21:14 +0000

    🏠 Update dashboard to show galleries, albums, and photos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   def index
     @bookmarks_count = Current.session.user.bookmarks.count
+    @galleries_count = Current.session.user.galleries.count
+    @albums_count = Current.session.user.albums.count
+    @photos_count = Current.session.user.photos.count
   end
 end
