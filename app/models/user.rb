645038b3ff02bc9ba6280f189f8628f73d13bd94
Stephen McCullough <me@swm.cc>
2025-11-13 22:17:25 +0000

    👤 Add gallery/album/photo associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  has_many :galleries, dependent: :destroy
+  has_many :albums, dependent: :destroy
+  has_many :photos, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
